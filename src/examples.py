833a911eafb53a4a38eb33950a3ab2d58ef6ebc6
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-03 19:53:52 +0100

    Add more examples for data functions

--- src/examples.py
+++ src/examples.py
@@ -1,7 +1,16 @@
 from datetime import datetime
 from models import Transaction
 from reader import read_data
-from data import Filter, TagLists, get_all_tags, get_transactions_for_tags, Aggregate
+from data import (
+    Filter,
+    TagLists,
+    get_all_tags,
+    get_all_transactions,
+    get_tags,
+    get_transactions_for_tags,
+    Aggregate,
+    group_transaction_by_tag_level,
+)
 import database
 
 
@@ -45,6 +54,16 @@ def misc():
     print("\nget_all_tags:")
     for tag in get_all_tags():
         print(tag)
+
+    print("\nget_all_tags(first 10 transactions)")
+    for tag in get_tags(get_all_transactions()[0:10]):
+        print(tag)
+
+    print("\ngroup_transaction_by_tag_level(first 10 transactions)")
+    groups = group_transaction_by_tag_level(get_all_transactions()[0:10])
+    print("L1: ", [transaction.original_description for transaction in groups.L1])
+    print("L2: ", [transaction.original_description for transaction in groups.L2])
+    print("L3: ", [transaction.original_description for transaction in groups.L3])
 
 
 def examples():
