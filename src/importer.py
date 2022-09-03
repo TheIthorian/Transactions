f15622105994267deb39cdabcca9d337e07361e6
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-03 19:36:55 +0100

    Add main call

--- src/importer.py
+++ src/importer.py
@@ -5,7 +5,6 @@
 
 from data import get_all_transactions
 from database import connect
-from models import Transaction
 from reader import read_data
 
 
@@ -16,8 +15,15 @@ def add_new_transactions():
 
     # Only add new transactions
     conn = connect()
+    count = 1
     for transaction in transactions:
         if not transaction in existing_transactions:
             transaction.insert(conn)
+            count += 1
 
     conn.commit()
+    print(f"{count} new transactions added")
+
+
+if __name__ == "__main__":
+    add_new_transactions()
