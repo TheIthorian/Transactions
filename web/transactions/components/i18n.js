3ba2498eac59f3f4a4b4bfbebe5055db7a3c0bf4
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-04 11:27:54 +0100

    Add i18n

---
+++ web/transactions/components/i18n.js
@@ -0,0 +1,9 @@
+export const LABELS = {
+    transactionListGridTitle: 'Transactions',
+    accountName: 'Account',
+    transactionDate: 'Date',
+    transactionDescription: 'Description',
+    transactionAmount: 'Amount',
+    transactionTag: 'Tag',
+    transactionListGridTitle: 'Transactions',
+};
