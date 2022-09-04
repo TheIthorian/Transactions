06feae371415975d0b5795bb2dae0673d9ed4811
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-04 11:47:20 +0100

    Add transaction list to home

--- web/transactions/pages/index.js
+++ web/transactions/pages/index.js
@@ -1,9 +1,11 @@
-import Breakdown from '../components/breakdown';
-import Layout from '../components/layout';
+import Breakdown from 'components/breakdown';
+import Layout from 'components/layout';
+import { TransactionList } from 'components/transaction-list';
 
 export default function Home() {
     return (
         <Layout>
+            <TransactionList />
             <Breakdown />
         </Layout>
     );
