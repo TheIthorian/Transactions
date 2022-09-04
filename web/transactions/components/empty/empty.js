ada7ef460827a3b0989b62233c44b71e67eb659d
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-04 10:50:08 +0100

    Import error component from other project

---
+++ web/transactions/components/empty/empty.js
@@ -0,0 +1,5 @@
+import { Empty } from 'antd';
+
+export default function () {
+    return <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />;
+}
---
+++ web/transactions/components/error/error.js
@@ -0,0 +1,10 @@
+import propTypes from 'prop-types';
+import { Alert } from 'antd';
+
+export function Error({ error }) {
+    return <Alert message={error.title} description={error.message} type='error' showIcon />;
+}
+
+Error.prototypes = {
+    error: propTypes.object,
+};
