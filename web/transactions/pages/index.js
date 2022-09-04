0b5464459a2d54a81a193fcc4d6d019991854931
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-04 15:46:38 +0100

    Add toast handler

--- web/transactions/pages/index.js
+++ web/transactions/pages/index.js
@@ -1,10 +1,12 @@
 import Breakdown from 'components/breakdown';
 import Layout from 'components/layout';
 import { TransactionList } from 'components/transaction-list';
+import { ToastHandler } from 'components/toast-handler';
 
 export default function Home() {
     return (
         <Layout>
+            <ToastHandler />
             <TransactionList />
             <Breakdown />
         </Layout>
