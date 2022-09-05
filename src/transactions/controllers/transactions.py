e102c2dddde61eed39ea5af1ffc4ed096d66fc2c
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-05 23:26:35 +0100

    Add logic for get_transactions

--- src/transactions/controllers/transactions.py
+++ src/transactions/controllers/transactions.py
@@ -1,9 +1,21 @@
 from transactions.http.request import Request
 from transactions.schema.transaction_schema import TransactionFilter
 
+from transactions.data import get_transactions_for_tags, Filter
 
-def get_transactions(filter: TransactionFilter, request: Request) -> dict:
+
+def get_transactions(filter: TransactionFilter, request: Request) -> list:
     print(filter)
 
-    data = [{"id": 10, "account": filter.account}]
-    return data
+    filtered_transactions = get_transactions_for_tags(filter.tags)
+    filtered_transactions = Filter.filter_by_date(
+        filtered_transactions, start_date=filter.date_from, end_date=filter.date_to
+    )
+    filtered_transactions = Filter.filter_by_account(
+        filtered_transactions, filter.account
+    )
+    filtered_transactions = Filter.filter_by_value(
+        filtered_transactions, filter.min_value, filter.max_value
+    )
+
+    return filtered_transactions
