5dd201007ca84b04ea077653f7378b83f3c1bad5
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-05 20:58:46 +0100

    Add route register

--- src/transactions/http/health.py
+++ src/transactions/http/health.py
@@ -1,3 +1,17 @@
 def hello_world():
     output = {"Hello": "Hello", "World": "World"}
     return output
+
+
+def status():
+    return {"status": "active"}
+
+
+def register_routes(app):
+    @app.route("/hello-world", methods=["GET"])
+    def _hello_world():
+        return hello_world()
+
+    @app.route("/health", methods=["GET", "POST"])
+    def _health():
+        return status()
