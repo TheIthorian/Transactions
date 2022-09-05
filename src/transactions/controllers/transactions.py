dca8d0b4bd42872d92d391dfa9793990a388bd08
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-05 21:44:39 +0100

    Remove unused import

--- src/transactions/controllers/transactions.py
+++ src/transactions/controllers/transactions.py
@@ -1,14 +1,8 @@
-from transactions.http.response import Response
 from transactions.schema.transaction_schema import TransactionFilter
 
 
 def get_transactions(filter: TransactionFilter) -> dict:
-    print(filter.account)
-    print(filter.date_from)
-    print(filter.date_to)
-    print(filter.min_value)
-    print(filter.max_value)
-    print(filter.tags)
+    print(filter)
 
     data = [{"id": 10, "account": filter.account}]
     return data
