4fa6336537b8a8f3a70bb2d3ba7c1f43a5d273de
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-05 22:12:59 +0100

    Use factory function for not_found_error

--- src/transactions/routes/__init__.py
+++ src/transactions/routes/__init__.py
@@ -1,8 +1,10 @@
+from flask import request
+
 from transactions.routes import health, transactions
-from transactions.routes.error import not_found_error
+from transactions.routes.error import make_not_found_error
 
 # https://stackoverflow.com/questions/17129573/can-i-use-external-methods-as-route-decorators-in-python-flask
 def register_routes(app):
-    app.register_error_handler(404, not_found_error)
+    app.register_error_handler(404, make_not_found_error(request))
     health.register_routes(app)
     transactions.register_routes(app)
--- src/transactions/routes/error.py
+++ src/transactions/routes/error.py
@@ -1,11 +1,14 @@
-from flask import jsonify, request
+from flask import jsonify
 
 
-def not_found_error(error):
-    message = {
-        "status": 404,
-        "message": "Record not found: " + request.url,
-    }
-    respone = jsonify(message)
-    respone.status_code = 404
-    return respone
+def make_not_found_error(request):
+    def not_found_error(error):
+        message = {
+            "status": 404,
+            "message": "Record not found: " + request.url,
+        }
+        respone = jsonify(message)
+        respone.status_code = 404
+        return respone
+
+    return not_found_error
