a4a1f0b461e0089f9e86edefc47c879e2a5898d5
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-06 22:39:14 +0100

    Add get_all_tags

--- src/transactions/controllers/transactions.py
+++ src/transactions/controllers/transactions.py
@@ -1,19 +1,24 @@
 from transactions.http.request import Request
 from transactions.schema.transaction_schema import TransactionFilter
 
-from transactions.data import get_transactions_for_tags, Filter
+from transactions import data
 
 
 def get_transactions(filter: TransactionFilter, request: Request) -> list:
-    filtered_transactions = get_transactions_for_tags(filter.tags)
-    filtered_transactions = Filter.filter_by_date(
+    filtered_transactions = data.get_transactions_for_tags(filter.tags)
+    filtered_transactions = data.Filter.filter_by_date(
         filtered_transactions, start_date=filter.date_from, end_date=filter.date_to
     )
-    filtered_transactions = Filter.filter_by_account(
+    filtered_transactions = data.Filter.filter_by_account(
         filtered_transactions, filter.account
     )
-    filtered_transactions = Filter.filter_by_value(
+    filtered_transactions = data.Filter.filter_by_value(
         filtered_transactions, filter.min_value, filter.max_value
     )
 
     return filtered_transactions
+
+
+def get_all_tags(request: Request) -> list:
+    # print(data.get_all_tags.cache_info())
+    return data.get_all_tags()
