b8e8797e225e2adefb38f5bc85f0796edb74d2d0
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-06 21:49:29 +0100

    Comment out unneeded headers

--- src/transactions/http/response.py
+++ src/transactions/http/response.py
@@ -1,3 +1,6 @@
+from transactions.config import CONFIG
+
+
 class Response:
     content_type = {"Content-Type": "application/json"}
 
@@ -20,18 +23,18 @@ def register_after_request(app):
 
 
 def add_cors(response):
-    response.headers.set("Access-Control-Allow-Origin", "*")
+    response.headers.set("Access-Control-Allow-Origin", CONFIG.REQUEST_ORIGIN)
     response.headers["Access-Control-Allow-Request-Headers"] = "*"
     response.headers["Access-Control-Allow-Methods"] = "GET, HEAD, OPTIONS, POST"
-    response.headers.add("Access-Control-Allow-Headers", "Access-Control-Allow-Headers")
-    response.headers.add("Access-Control-Allow-Headers", "Origin")
-    response.headers.add("Access-Control-Allow-Headers", "Content-Type")
-    response.headers.add(
-        "Access-Control-Allow-Headers", "Access-Control-Request-Method"
-    )
-    response.headers.add(
-        "Access-Control-Allow-Headers", "Access-Control-Request-Headers"
-    )
+    # response.headers.add("Access-Control-Allow-Headers", "Access-Control-Allow-Headers")
+    # response.headers.add("Access-Control-Allow-Headers", "Origin")
+    # response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+    # response.headers.add(
+    #     "Access-Control-Allow-Headers", "Access-Control-Request-Method"
+    # )
+    # response.headers.add(
+    #     "Access-Control-Allow-Headers", "Access-Control-Request-Headers"
+    # )
     response.headers.add("Access-Control-Allow-Headers", "api_key")
 
     return response
