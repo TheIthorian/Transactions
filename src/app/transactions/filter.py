259dd3ae3de394b9f4a86f98306be8827bbeaec9
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-10 00:14:28 +0100

    filter_tags_by tag level functions

--- src/app/transactions/filter.py
+++ src/app/transactions/filter.py
@@ -17,6 +17,18 @@ def filter_transactions(transactions: list[Transaction], filter: TransactionFilt
     return filtered_transactions
 
 
+def filter_tags_by_l1(tags: list[Tag], l1_value: str):
+    return list(filter(lambda t: t.l1 == l1_value, tags))
+
+
+def filter_tags_by_l2(tags: list[Tag], l2_value: str):
+    return list(filter(lambda t: t.l2 == l2_value, tags))
+
+
+def filter_tags_by_l3(tags: list[Tag], l3_value: str):
+    return list(filter(lambda t: t.l3 == l3_value, tags))
+
+
 def filter_by_tag(
     transactions: list[Transaction], l1: Tag = None, l2: Tag = None, l3: Tag = None
 ) -> list[Transaction]:
