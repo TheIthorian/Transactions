178d6d69d360bea1e191c07a86457b088c293b27
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-14 18:23:21 +0100

    Remove old get_breakdown_by_tag function

--- src/app/transactions/breakdown.py
+++ src/app/transactions/breakdown.py
@@ -7,7 +7,8 @@ from app.transactions.transaction_model import TransactionsByTagLevel
 from app.util.list import unique
 
 
-def get_transaction_amounts_by_tag_level(level: int):
+def get_transaction_amounts_by_tag_level(level: int) -> Tuple[str, int]:
+    """Returns a tuple of `(tag_name, amount)` for the given tag level"""
     if level == 1:
         query = """SELECT SUM(amount) AS amount, l1 FROM transactions GROUP BY l1 ORDER BY l1"""
     elif level == 2:
@@ -17,66 +18,3 @@ def get_transaction_amounts_by_tag_level(level: int):
 
     result = database.select(query)
     return [(r[level], r[0]) for r in result]
-
-
-def get_breakdown_by_tag(
-    transactions_by_tag_level: TransactionsByTagLevel,
-) -> Tuple[list, list, list]:
-    print(transactions_by_tag_level)
-    l1_data: list[Tuple] = []
-    all_l1_tags = data.get_tags_from_transactions(transactions_by_tag_level.l1)
-    unique_l1_tag_names = unique(map(lambda t: t.l1, all_l1_tags))
-    for tag_name in unique_l1_tag_names:
-        tag_amount = aggregate(
-            transactions_by_tag_level.l1,
-            lambda t: t.tag.l1 == tag_name,
-            lambda t: t.amount,
-            0,
-        )
-        l1_data.append((tag_name, tag_amount))
-
-    l2_data: list[Tuple] = []  # all l2 tags, split by their l1 parent
-    all_l2_tags = data.get_tags_from_transactions(transactions_by_tag_level.l2)
-    for l1_tag, l1_total_amount in l1_data:
-        l1_sum = 0
-        unique_l2_tag_names = unique(
-            map(lambda t: t.l2, filter_tags_by_l1(all_l2_tags, l1_tag))
-        )
-        for tag in unique_l2_tag_names:
-            tag_amount = aggregate(
-                transactions_by_tag_level.l2,
-                lambda t: t.tag.l2 == tag and t.tag.l2 != "",
-                lambda t: t.amount,
-                0,
-            )
-            l1_sum += tag_amount
-            l2_data.append((tag, tag_amount))
-
-        l2_data.append(("None", l1_total_amount - l1_sum))
-
-    l3_data: list[list[list]] = []  # all l3 tags, split by their l2 parent
-    all_l3_tags = data.get_tags_from_transactions(transactions_by_tag_level.l3)
-    for l1_tags in l2_data:
-        l3_tag_data: list[list] = []
-
-        if len(l1_tags) == 0:
-            l3_tag_data = [[]]
-
-        for l2_tag, _ in l1_tags:
-            l3_tags_for_l2: list[Tuple] = []
-            for l3_tag in unique(
-                map(lambda t: t.l3, filter_tags_by_l2(all_l3_tags, l2_tag))
-            ):
-                tag_amount = aggregate(
-                    transactions_by_tag_level.l3,
-                    lambda t: t.tag.l3 == l3_tag and t.tag.l3 != "",
-                    lambda t: t.amount,
-                    0,
-                )
-                l3_tags_for_l2.append((l3_tag, tag_amount))
-
-            l3_tag_data.append(l3_tags_for_l2)
-
-        l3_data.append(l3_tag_data)
-
-    return l1_data, l2_data, l3_data
