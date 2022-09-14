cbd44e898b62a4a41d44d2310ad202aeda5ec352
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-14 22:48:01 +0100

    Wrap toolbar elements

--- web/transactions/components/toolbar/toolbar.js
+++ web/transactions/components/toolbar/toolbar.js
@@ -5,7 +5,7 @@ import styles from './toolbar.module.css';
 
 export default function Toolbar({ title, children }) {
     return (
-        <Space width='100%' align='baseline' className={styles.toolbar}>
+        <Space width='100%' align='baseline' className={styles.toolbar} wrap>
             <h1>{title}</h1>
             <Space style={{ width: '100%', justifyContent: 'end' }}>{children}</Space>
         </Space>
