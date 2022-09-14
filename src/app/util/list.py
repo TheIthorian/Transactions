a12f8884cb389ece6e9a90885c06940a6b89d248
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-14 17:52:26 +0100

    Support unhashable unique lists

--- src/app/util/list.py
+++ src/app/util/list.py
@@ -1,7 +1,23 @@
+import collections
 from typing import Iterable
 
 
 def unique(sequence: Iterable) -> list:
+    if len(sequence) == 0:
+        return []
+
+    if isinstance(sequence[0], collections.Hashable):
+        return unique_with_set(sequence)
+
+    return unique_without_set(sequence)
+
+
+def unique_with_set(sequence: Iterable) -> list:
     seen = set()
     seen_add = seen.add
     return [x for x in sequence if not (x in seen or seen_add(x))]
+
+
+def unique_without_set(sequence: Iterable) -> list:
+    seen = []
+    return [x for x in sequence if not (x in seen or seen.append(x))]
