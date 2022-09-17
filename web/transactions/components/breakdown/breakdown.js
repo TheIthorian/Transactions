ff412173c6041aa281124c4d7e81c71aed597f88
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-17 17:01:38 +0100

    Add mode selection and fix date persistence

--- web/transactions/components/breakdown/breakdown.js
+++ web/transactions/components/breakdown/breakdown.js
@@ -1,4 +1,4 @@
-import { Button, Card, Divider, Select, DatePicker, Skeleton, Empty } from 'antd';
+import { Button, Card, Divider, Select, DatePicker, Skeleton, Empty, Radio, Space } from 'antd';
 const { Option } = Select;
 
 import { useEffect, useState } from 'react';
@@ -12,27 +12,28 @@ import { LABELS } from 'components/i18n';
 import { makeStore } from 'util/store';
 
 import { getAllTags, getBreakdown } from './data';
-import { STORE_ID } from './constants';
+import { DEFAULT_MODE, MODES, STORE_ID } from './constants';
 
 export default function Breakdown() {
+    const store = makeStore(STORE_ID);
+
     const [loaded, setLoaded] = useState(true);
     const [data, setData] = useState(null);
-    const [filter, setFilter] = useState(undefined);
-    const [reload, setReload] = useState(false);
-    const [allTags, setAllTags] = useState([]);
-    const store = makeStore(STORE_ID);
-    const dateFormat = 'YYYY-MM-DD';
-    const defaultDateFrom = store.get('dateTo')
-        ? moment(store.get('dateTo'), dateFormat)
-        : undefined;
-
-    const defaultDateTo = store.get('dateTo') ? moment(store.get('dateTo'), dateFormat) : undefined;
-
-    const storedFilter = {
+    const [filter, setFilter] = useState({
         dat_from: store.get('dateFrom'),
         date_to: store.get('dateTo'),
         tags: { l1: store.get('tagFilter') },
-    };
+    });
+    const [reload, setReload] = useState(false);
+    const [allTags, setAllTags] = useState([]);
+    const [mode, setMode] = useState(store.get('mode') ?? DEFAULT_MODE);
+
+    const dateFormat = 'YYYY-MM-DD';
+    const defaultDateFrom = store.get('dateFrom')
+        ? moment(store.get('dateFrom'), dateFormat)
+        : undefined;
+
+    const defaultDateTo = store.get('dateTo') ? moment(store.get('dateTo'), dateFormat) : undefined;
 
     useEffect(() => {
         console.log('useEffect 2');
@@ -40,7 +41,7 @@ export default function Breakdown() {
             setAllTags(res);
         });
 
-        getBreakdown(filter ?? storedFilter)
+        getBreakdown(filter)
             .then(setData)
             .then(() => setLoaded(true))
             .catch(console.log);
@@ -74,6 +75,13 @@ export default function Breakdown() {
         setFilter(filter => ({ ...filter, date_to }));
     }
 
+    function handleOnChangeMode(event) {
+        const value = event.target.value;
+        store.set('mode', value);
+        console.log(value);
+        setMode(value);
+    }
+
     function isLoading() {
         return !allTags.length || !data || !loaded;
     }
@@ -97,23 +105,19 @@ export default function Breakdown() {
                     style={{
                         display: 'flex',
                         justifyItems: 'end',
-                        justifyContent: ' space-around',
+                        justifyContent: 'space-around',
                     }}
                 >
                     <DatePicker
                         onChange={handleChangeDateFrom}
-                        style={{
-                            marginRight: '5px',
-                        }}
+                        style={{ marginRight: '5px' }}
                         defaultValue={defaultDateFrom}
                     >
                         From
                     </DatePicker>
                     <DatePicker
                         onChange={handleChangeDateTo}
-                        style={{
-                            marginRight: '5px',
-                        }}
+                        style={{ marginRight: '5px' }}
                         defaultValue={defaultDateTo}
                     >
                         To
@@ -135,10 +139,15 @@ export default function Breakdown() {
                     <Button onClick={handleReload}>
                         <ReloadOutlined />
                     </Button>
+                    <Radio.Group onChange={handleOnChangeMode} value={mode}>
+                        <Space direction='vertical' style={{ padding: 10 }}>
+                            <Radio value={MODES.All}>{MODES.All}</Radio>
+                            <Radio value={MODES.Monthly}>{MODES.Monthly}</Radio>
+                        </Space>
+                    </Radio.Group>
                 </div>
             </Toolbar>
             <Divider style={{ margin: '0 0 10px' }} />
-
             <Skeleton loading={isLoading()}>
                 {isEmpty() ? (
                     <Empty />
--- web/transactions/components/breakdown/constants.js
+++ web/transactions/components/breakdown/constants.js
@@ -1,1 +1,6 @@
 export const STORE_ID = 'breakdown-chart';
+export const MODES = {
+    All: 'All',
+    Monthly: 'Monthly',
+};
+export const DEFAULT_MODE = MODES.All;
