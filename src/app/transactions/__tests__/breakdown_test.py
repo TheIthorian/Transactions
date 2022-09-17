cd6398e9d5d2b716c9f6d0926b73250b3c9f8aff
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-17 11:42:59 +0100

    Update tests to use mock database

--- src/app/transactions/__tests__/breakdown_test.py
+++ src/app/transactions/__tests__/breakdown_test.py
@@ -10,49 +10,81 @@ from app.transactions.transaction_model import Transaction
 FILTER = TransactionFilter()
 
 
-class Test_database:
-    def test_select(self):
-        database.mock()
+def make_transaction():
+    account = "test account name"
+    date = datetime(2022, 1, 1).date()
+    current_description = "current description"
+    original_description = "original description"
+    amount = 0
 
-        transactions = database.select("SELECT * FROM Transactions LIMIT 1")
-        print(transactions)
+    return Transaction(
+        account=account,
+        date=date,
+        current_description=current_description,
+        original_description=original_description,
+        amount=amount,
+        tag=Tag(l1="", l2="", l3=""),
+    )
 
-        transaction = Transaction.make(
-            account="Test Account",
-            date=datetime.now(),
-            current_description="Test Description",
-            original_description="",
-            amount=69,
-            tag=Tag(l1="Test", l2="", l3=""),
-        )
 
-        transaction.insert()
+def setup():
+    transactions = [make_transaction() for _ in range(7)]
 
-        transactions = database.select("SELECT * FROM Transactions LIMIT 1")
-        print(transactions)
+    # Income
+    transactions[0].amount = 10_000
+    transactions[0].tag.l1 = "Income"
 
-        database.unmock()
+    transactions[1].amount = 10_000
+    transactions[1].tag.l1 = "Income"
+
+    # Home
+    transactions[2].amount = -10_000
+    transactions[2].tag.l1 = "Home"
+    transactions[2].tag.l2 = "Bills"
+
+    transactions[3].amount = -5_000
+    transactions[3].tag.l1 = "Home"
+    transactions[3].tag.l2 = "Other"
+
+    # Appearance
+    transactions[4].amount = -4_000
+    transactions[4].tag.l1 = "Appearance"
+    transactions[4].tag.l2 = "Clothes"
+    transactions[4].tag.l3 = "Everyday"
+
+    transactions[5].amount = -3_000
+    transactions[5].tag.l1 = "Appearance"
+    transactions[5].tag.l2 = "Clothes"
+    transactions[5].tag.l2 = "Work"
+
+    # Enjoyment
+    transactions[6].amount = -2_000
+    transactions[6].tag.l1 = "Enjoyment"
+    transactions[6].tag.l2 = "Eating out"
+    transactions[6].tag.l2 = "Everyday"
+
+    return transactions
 
 
 class Test_get_breakdown_by_tag:
-    def test_returns_grouped_l1_tags(self, mocker):
-        # given
-        mock_query_results = [
-            [20_000, "Income"],
-            [-15_000, "Home"],
-            [-7_000, "Appearance"],
-            [-2_000, "Enjoyment"],
-        ]
+    def test_returns_grouped_l1_tags(self):
+        database.mock()
 
-        mocker.patch("app.database.select", return_value=mock_query_results)
+        # Given
+        transactions = setup()
+        for transaction in transactions:
+            transaction.insert()
 
+        # When
         result = get_transaction_amounts_by_tag_level(1, FILTER)
+        database.unmock()
 
+        # Then
         assert result == [
-            ("Income", 20_000),
-            ("Home", -15_000),
-            ("Appearance", -7_000),
-            ("Enjoyment", -2_000),
+            ("Appearance", -7_000.0),
+            ("Enjoyment", -2_000.0),
+            ("Home", -15_000.0),
+            ("Income", 20_000.0),
         ]
 
     def test_returns_grouped_l2_tags(self, mocker):
