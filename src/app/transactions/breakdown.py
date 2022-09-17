539f7c643beb700d66c3137e7e1710a5aced06ad
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-17 16:46:19 +0100

    Simplify breakdown query logic

--- src/app/transactions/breakdown.py
+++ src/app/transactions/breakdown.py
@@ -37,73 +37,28 @@ def get_average_transaction_amounts_by_tag_level(
     )
     inputs = query_builder.get_inputs()
 
+    tag_columns = ", ".join(["l1", "l2", "l3"][0:level])
     year_column = "strftime('%Y', datetime( t.date, 'unixepoch' ))"
     month_column = "strftime('%m', datetime( t.date, 'unixepoch' ))"
 
-    if level == 1:
-        query = f"""
-        SELECT AvG(SumByDate.amount) as AverageAmount, SumByDate.l1
-        FROM (SELECT
-                SUM(t.amount) AS amount, 
-                t.l1,
-                {year_column} As year, 
-                {month_column} as month
-            FROM transactions t
-            {condition}
-            GROUP BY t.l1,
-                {year_column}, 
-                {month_column}
-            ) AS SumByDate
-        GROUP BY SumByDate.l1
-        """
-    elif level == 2:
-        query = f"""
-        SELECT AvG(SumByDate.amount) as AverageAmount, 
-            SumByDate.l1, 
-            SumByDate.l2
-        FROM (SELECT
-                SUM(t.amount) AS amount, 
-                t.l1,
-                t.l2,
-                {year_column} As year, 
-                {month_column} as month
-            FROM transactions t
-            {condition}
-            GROUP BY t.l1,
-                t.l2,
-                {year_column}, 
-                {month_column}
-            ) AS SumByDate
+    query = f"""
+    SELECT AvG(SumByDate.amount) as AverageAmount, 
+        {tag_columns}
+    FROM (SELECT
+            SUM(t.amount) AS amount, 
+            {tag_columns},
+            {year_column} As year, 
+            {month_column} as month
+        FROM transactions t
+        {condition}
         GROUP BY 
-            SumByDate.l1,
-            SumByDate.l2
-        """
-    elif level == 3:
-        query = f"""
-        SELECT AvG(SumByDate.amount) as AverageAmount, 
-            SumByDate.l1, 
-            SumByDate.l2,
-            SumByDate.l3
-        FROM (SELECT
-                SUM(t.amount) AS amount, 
-                t.l1,
-                t.l2,
-                t.l3,
-                {year_column} As year, 
-                {month_column} as month
-            FROM transactions t
-            {condition}
-            GROUP BY t.l1,
-                t.l2,
-                t.l3,
-                {year_column}, 
-                {month_column}
-            ) AS SumByDate
-        GROUP BY 
-            SumByDate.l1,
-            SumByDate.l2,
-            SumByDate.l3
-        """
+            {tag_columns},
+            {year_column}, 
+            {month_column}
+        ) AS SumByDate
+    GROUP BY 
+        {tag_columns}
+    """
 
     result = database.select(query, inputs)
     return [(r[level], r[0]) for r in result]
