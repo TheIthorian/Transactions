b9974d3be6c60fbabc811d89b3f292465161d0c3
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-18 12:06:39 +0100

    Add default l2 and l3 values

--- src/app/tags/tag_model.py
+++ src/app/tags/tag_model.py
@@ -27,7 +27,7 @@ class Tag:
     l3: str
     color: str = None
 
-    def __init__(self, l1, l2, l3):
+    def __init__(self, l1, l2="", l3=""):
         self.l1 = l1
         self.l2 = l2
         self.l3 = l3
