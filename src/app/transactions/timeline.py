05fc56a4dd9de5982858edc723891a892072f597
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-19 15:40:16 +0100

    Update get_transaction_timeline to handle tag grouping

--- src/app/transactions/timeline.py
+++ src/app/transactions/timeline.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from datetime import date, datetime
 from app import database
 from app.transactions.filter import TransactionFilter
+from app.transactions.transaction_model import Query
 
 
 @dataclass
@@ -21,21 +22,43 @@ class TimelineMonth:
 def get_transaction_timeline(
     filter: TransactionFilter, group_by_tag: bool = True
 ) -> list[TimelineMonth]:
-    qb = filter.build_query()
-    conditions = qb.build()
-    month_column = "strftime('%Y-%m', t.date, 'unixepoch')"
-    l1_column = ", t.l1" if group_by_tag else ""
+    qb = (
+        Query(alias="t.")
+        .by_account(filter.account)
+        .amount_from(filter.min_value)
+        .amount_to(filter.max_value)
+    )
+    conditions = qb.build_conditions(query="", include_where=False)
 
-    query = f"""SELECT SUM(t.amount) as total, {month_column} AS Month {l1_column}
-        FROM Transactions t 
-        {conditions}
+    date_qb = Query(alias="dt.").date_from(filter.date_from).date_to(filter.date_to)
+    date_conditions = date_qb.build()
+
+    tag_qb = Query(alias="tr.").by_tag_filter(filter.tags)
+    tag_conditions = tag_qb.build()
+
+    l1_column = ", tags.l1" if group_by_tag else ""
+
+    query = f"""SELECT 
+            SUM(COALESCE(t.amount, 0)) as total, 
+            months.month
+            {l1_column}
+        FROM 
+        (SELECT DISTINCT tr.l1 FROM transactions tr {tag_conditions}) as tags,
+        (SELECT DISTINCT strftime('%Y-%m', dt.date, 'unixepoch') month from transactions dt {date_conditions}) as months
+        LEFT JOIN Transactions t 
+            ON t.l1 = tags.l1 
+            AND strftime('%Y-%m', t.date, 'unixepoch') = months.month
+            {conditions}
         GROUP BY 
-            {month_column} {l1_column}
+            months.month
+            {l1_column}
         ORDER BY 
-            {month_column} {l1_column}
-        """
+            months.month
+            {l1_column}"""
 
-    amounts = database.select(query, qb.get_inputs())
+    inputs = [*date_qb.get_inputs(), *qb.get_inputs(), *tag_qb.get_inputs()]
+
+    amounts = database.select(query, inputs)
 
     date_format = "%Y-%m"
     return [
