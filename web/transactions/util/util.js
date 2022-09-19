6c4cd2224be0c984cb9b7dc2bb531cd5e2940fce
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-19 17:54:04 +0100

    Remove uvl

--- web/transactions/util/util.js
+++ web/transactions/util/util.js
@@ -1,7 +1,3 @@
-export function uvl(value, _default) {
-    return value === undefined ? _default : value;
-}
-
 export const idMaker = (function* () {
     var index = -1;
     while (true) yield index--;
