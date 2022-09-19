39f55c5b824e698e1dc521a67b1da6846e2c6ae7
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-09-19 12:29:41 +0100

    Add tests for query filtering

--- src/app/transactions/__tests__/timeline_test.py
+++ src/app/transactions/__tests__/timeline_test.py
@@ -2,40 +2,38 @@ from datetime import datetime
 
 from app import database
 from app.tags.tag_model import Tag
-from app.transactions.filter import TransactionFilter
+from app.transactions.filter import TagFilter, TransactionFilter
 from app.transactions.timeline import TimelineMonth, get_transaction_timeline
 from app.transactions.transaction_model import Transaction
 
 
+def setup():
+    return [
+        Transaction.make(
+            date=datetime(2022, 2, 10), amount=10_000, tag=Tag(l1="Income")
+        ),
+        Transaction.make(
+            date=datetime(2022, 2, 10), amount=-2_000, tag=Tag(l1="Bills")
+        ),
+        Transaction.make(
+            date=datetime(2022, 3, 10), amount=10_000, tag=Tag(l1="Income")
+        ),
+        Transaction.make(date=datetime(2022, 3, 1), amount=-2_000, tag=Tag(l1="Bills")),
+        Transaction.make(date=datetime(2022, 3, 1), amount=-4_000, tag=Tag(l1="Bills")),
+    ]
+
+
 class Test_get_transaction_timeline:
-    def test_returns_total_out_and_in_over_each_month(self):
+    def test_returns_total_over_each_month(self):
         # Given
         database.mock()
 
-        transactions = [
-            Transaction.make(
-                date=datetime(2022, 2, 10), amount=10_000, tag=Tag(l1="Income")
-            ),
-            Transaction.make(
-                date=datetime(2022, 2, 10), amount=-2_000, tag=Tag(l1="Bills")
-            ),
-            Transaction.make(
-                date=datetime(2022, 3, 10), amount=10_000, tag=Tag(l1="Income")
-            ),
-            Transaction.make(
-                date=datetime(2022, 3, 1), amount=-2_000, tag=Tag(l1="Bills")
-            ),
-            Transaction.make(
-                date=datetime(2022, 3, 1), amount=-4_000, tag=Tag(l1="Bills")
-            ),
-        ]
-
+        transactions = setup()
         for transaction in transactions:
             transaction.insert()
 
         # When
         result = get_transaction_timeline(TransactionFilter())
-        print(result)
         database.unmock()
 
         # Then
@@ -50,6 +48,64 @@ class Test_get_transaction_timeline:
                 month_start_date=datetime(2022, 2, 1).date(),
                 l1="Income",
             ),
+            TimelineMonth(
+                amount=-6_000.0,
+                month_start_date=datetime(2022, 3, 1).date(),
+                l1="Bills",
+            ),
+            TimelineMonth(
+                amount=10_000.0,
+                month_start_date=datetime(2022, 3, 1).date(),
+                l1="Income",
+            ),
+        ]
+
+    def test_filters_transactions_by_tag(self):
+        # Given
+        database.mock()
+
+        transactions = setup()
+        for transaction in transactions:
+            transaction.insert()
+
+        # When
+        result = get_transaction_timeline(
+            TransactionFilter(tags=TagFilter(l1=["Income"]))
+        )
+        print(result)
+        database.unmock()
+
+        # Then
+        assert result == [
+            TimelineMonth(
+                amount=10_000.0,
+                month_start_date=datetime(2022, 2, 1).date(),
+                l1="Income",
+            ),
+            TimelineMonth(
+                amount=10_000.0,
+                month_start_date=datetime(2022, 3, 1).date(),
+                l1="Income",
+            ),
+        ]
+
+    def test_filters_transactions_by_date(self):
+        # Given
+        database.mock()
+
+        transactions = setup()
+        for transaction in transactions:
+            transaction.insert()
+
+        # When
+        result = get_transaction_timeline(
+            TransactionFilter(date_from=datetime(2022, 3, 1).date())
+        )
+        print(result)
+        database.unmock()
+
+        # Then
+        assert result == [
             TimelineMonth(
                 amount=-6_000.0,
                 month_start_date=datetime(2022, 3, 1).date(),
