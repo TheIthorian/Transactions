3d394234d164c28561d3f28d015976803ce76441
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-11-04 12:20:11 +0000

    Update authentication tests

--- src/app/http/__tests__/authentication_test.py
+++ src/app/http/__tests__/authentication_test.py
@@ -1,7 +1,20 @@
-from app.http.authentication import is_key_valid
+from app.config import CONFIG
+from app.http.authentication import is_key_valid, is_password_valid
 
 
 class Test_is_key_valid:
-    def returns_true(self):
-        key = "Any key works"
-        assert is_key_valid(key)
+    def test_returns_false_for_invalid_key(self):
+        key = "Incorrect key"
+        assert not is_key_valid(key)
+
+    def test_returns_true_for_correct_key(self):
+        assert is_key_valid(CONFIG.API_KEY)
+
+
+class Test_is_password_valid:
+    def test_returns_false_for_invalid_password(self):
+        password = "Incorrect password"
+        assert not is_password_valid(password)
+
+    def test_returns_true_for_correct_key(self):
+        assert is_password_valid(CONFIG.PASSWORD)
