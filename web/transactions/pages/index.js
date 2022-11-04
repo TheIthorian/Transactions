d99531a42ac1f80b5b4981d823547c355c0e6e1d
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-11-04 14:59:29 +0000

    Add userContext and logout to home page

--- web/transactions/pages/index.js
+++ web/transactions/pages/index.js
@@ -3,14 +3,19 @@ import Layout from 'components/layout';
 import { TransactionList } from 'components/transaction-list';
 import { ToastHandler } from 'components/toast-handler';
 import { TransactionTimeline } from 'components/transaction-timeline';
+import { CurrentUserProvider } from 'hooks/userContext';
+import { Logout } from 'components/logout';
 
 export default function Home() {
     return (
-        <Layout>
-            <ToastHandler />
-            <TransactionList />
-            <Breakdown />
-            <TransactionTimeline />
-        </Layout>
+        <CurrentUserProvider>
+            <Layout>
+                <ToastHandler />
+                <Logout />
+                <TransactionList />
+                <Breakdown />
+                <TransactionTimeline />
+            </Layout>
+        </CurrentUserProvider>
     );
 }
