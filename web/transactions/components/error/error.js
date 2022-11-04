8683bd358567157ba0f3f65f2d5e985cf8daddb0
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-11-04 14:58:50 +0000

    Use optional chaining on error props

--- web/transactions/components/error/error.js
+++ web/transactions/components/error/error.js
@@ -2,7 +2,7 @@ import propTypes from 'prop-types';
 import { Alert } from 'antd';
 
 export function Error({ error }) {
-    return <Alert message={error.title} description={error.message} type='error' showIcon />;
+    return <Alert message={error?.title} description={error?.message} type='error' showIcon />;
 }
 
 Error.prototypes = {
