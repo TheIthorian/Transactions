2828b294aef7a7ccbebba500c3cf9d09906f7ab1
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-11-04 12:20:04 +0000

    Add function to check password

--- src/app/http/authentication.py
+++ src/app/http/authentication.py
@@ -3,3 +3,7 @@ from app.config import CONFIG
 
 def is_key_valid(key: str) -> bool:
     return key is not None and key == CONFIG.API_KEY
+
+
+def is_password_valid(password: str) -> bool:
+    return password == CONFIG.PASSWORD
