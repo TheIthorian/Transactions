07b7e4a362d08db7410c1d8fa37e94ef1eecc482
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-11-04 15:35:04 +0000

    Register user route

--- src/app/routes/__init__.py
+++ src/app/routes/__init__.py
@@ -2,12 +2,13 @@ from flask import request
 
 from app.routes import health
 from app.routes.error import make_not_found_error
-from app import transactions, tags
+from app import transactions, tags, user
 
 # https://stackoverflow.com/questions/17129573/can-i-use-external-methods-as-route-decorators-in-python-flask
 def register_routes(app):
     app.register_error_handler(404, make_not_found_error(request))
     health.register_routes(app)
 
+    user.register_routes(app)
     transactions.register_routes(app)
     tags.register_routes(app)
