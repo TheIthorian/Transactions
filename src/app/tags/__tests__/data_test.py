b66d37f2313d57a69299a1b01e88ae6b772c1312
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-01-14 14:25:42 +0000

    Use clean_mock in data_test

--- src/app/tags/__tests__/data_test.py
+++ src/app/tags/__tests__/data_test.py
@@ -3,6 +3,10 @@ from app import database
 from app.tags.data import get_all_tags
 from app.tags.tag_model import Tag
 from app.transactions.transaction_model import Transaction
+from app.config import CONFIG
+
+CONFIG.DATABASE_PATH = CONFIG.MOCK_DATABASE_PATH
+database.init()
 
 
 class Test_get_all_tags:
@@ -17,13 +21,12 @@ class Test_get_all_tags:
             Transaction.make(tag=Tag(l1="Home", l2="Bills", l3="Other")),
         ]
 
-        database.mock()
         for transaction in transactions:
             transaction.insert()
 
         # When
         result = get_all_tags()
-        database.unmock()
+        database.clean_mock()
 
         # Then
         assert result == [
