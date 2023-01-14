96d9528ee3cfd159f22af29fb6c1fd56910c1b2c
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-01-14 12:22:24 +0000

    Set user session on login

--- src/app/user/user_controller.py
+++ src/app/user/user_controller.py
@@ -1,8 +1,15 @@
 from app.http.request import Request
 from app.user.user_schema import LoginRequest
 from app.http.authentication import is_password_valid
+from app.user.user_session import make_user_session
 
 
 def login(input: LoginRequest, request: Request = None):
     is_password_correct = is_password_valid(input.password)
+
+    session_id = ""
+    if is_password_correct:
+        session_id = make_user_session()
+        request.set_cookie("session_id", session_id)
+
     return {"logged_in": is_password_correct}
