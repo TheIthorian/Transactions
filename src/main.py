c88d9843468a15388ae74b8d2dc09d9e0146b971
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-01-14 12:23:07 +0000

    Init database on runtime

--- src/main.py
+++ src/main.py
@@ -5,4 +5,8 @@ from app.http import app
 if __name__ == "__main__":
     routes.register_routes(app)
 
+    from app.database import init
+
+    init()
+
     app.run(debug=CONFIG.DEV, host=CONFIG.HOST)
