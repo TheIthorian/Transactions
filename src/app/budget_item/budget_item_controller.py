8d94c196e8d1cab0b2d6dfea2b7b513858f06570
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-01-15 19:57:04 +0000

    Add budget_item controller

---
+++ src/app/budget_item/budget_item_controller.py
@@ -0,0 +1,54 @@
+from app.http.request import Request, Error
+import app.database as database
+
+from app.budget_item.budget_item_model import BudgetItem
+from app.budget_item.budget_item_schema import BudgetItemRequestBody
+from app.budget_item.budget_item_service import (
+    select_item_by_id,
+    select_by_budget_id,
+    budget_item_exists,
+)
+
+
+def get_budget_items_for_budget(budget_id: int, request: Request = None):
+    budget_items = select_by_budget_id(budget_id)
+    return [BudgetItem.from_db(budget_item) for budget_item in budget_items]
+
+
+def get_budget_item(data: BudgetItemRequestBody, request: Request = None):
+    budgets = select_item_by_id(data.id, data.budget_id)
+
+    if len(budgets) == 0:
+        request.errors.append(item_not_found_error())
+        return
+
+    return BudgetItem.from_db(budgets[0])
+
+
+def add_budget_item(budget_item: BudgetItem, request: Request = None):
+    if budget_item_exists(budget_item.budget_id, budget_item.l1):
+        request.errors.append(already_exists_error())
+        return
+
+    budget_item.insert()
+    return budget_item
+
+
+def update_budget_item(budget_item: BudgetItem, request: Request = None):
+    budget_item.update()
+
+
+def already_exists_error():
+    return Error(
+        "Already exists",
+        "The budget for this tag already exists",
+        400,
+    )
+
+
+def item_not_found_error():
+    return Error(
+        "No Resource Found",
+        "The budget item you are looking for does not exist",
+        404,
+    )
