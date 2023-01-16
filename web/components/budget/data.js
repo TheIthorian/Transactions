2c505a711a838803a212b8641c68149d65e5cb62
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-01-16 20:03:34 +0000

    Use defaultRequest in data.js

--- web/components/budget/data.js
+++ web/components/budget/data.js
@@ -1,49 +1,34 @@
-import { API_URL, API_KEY } from 'config';
-import { handleResponse } from 'util/rest';
-import { makeStore } from 'util/store';
+import { API_URL } from 'config';
+import { handleResponse, defaultRequest } from 'util/rest';
 
 export async function getBudget(budgetId) {
-    const response = await fetch(API_URL + '/getBudget', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Api-Key': API_KEY,
-            session_id: makeStore('user').get('session_id'),
-        },
-        body: JSON.stringify({ budget_id: budgetId }), // This fails when null. Check error handling in Response.py
-    });
-
-    console.log(response);
+    const response = await fetch(API_URL + '/getBudget', defaultRequest({ budget_id: budgetId }));
 
     const data = await handleResponse(response);
-    return convertBudgetFromApi(data);
-}
-
-function convertBudgetFromApi(budget) {
     return {
-        ...budget,
-        key: budget.id,
+        ...data,
+        key: data.id,
     };
 }
 
 export async function getBudgetItems(budgetId) {
-    const response = await fetch(API_URL + '/getBudgetItems', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Api-Key': API_KEY,
-            session_id: makeStore('user').get('session_id'),
-        },
-        body: JSON.stringify({ budget_id: 1 }),
-    });
+    const response = await fetch(
+        API_URL + '/getBudgetItems',
+        defaultRequest({ budget_id: budgetId })
+    );
 
     const data = await handleResponse(response);
-    return data.map(convertBudgetItemFromApi);
+    return data.map(item => ({
+        ...item,
+        key: item.id,
+    }));
 }
 
-function convertBudgetItemFromApi(budgetItem) {
-    return {
-        ...budgetItem,
-        key: budgetItem.id,
-    };
+export async function addBudgetItem(budgetId, newItem) {
+    const response = await fetch(
+        API_URL + '/addBudgetItem',
+        defaultRequest({ budget_id: budgetId, l1: newItem.l1, amount: newItem.amount })
+    );
+
+    await handleResponse(response);
 }
