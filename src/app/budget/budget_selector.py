09858628b89c6ae151c8d59fb460310a1f24b70e
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-01-16 19:16:16 +0000

    Add get_tags_not_used_by_budget to selector

--- src/app/budget/budget_selector.py
+++ src/app/budget/budget_selector.py
@@ -12,3 +12,13 @@ def select_by_id(id) -> list:
         "SELECT budget_id, name, total_limit FROM Budget WHERE budget_id = :id",
         {"id": id},
     )
+
+
+def get_tags_not_used_by_budget(budget_id) -> list:
+    return database.select(
+        """SELECT DISTINCT T.l1 
+        FROM transactions T
+        WHERE T.l1 NOT IN (SELECT l1 FROM BudgetItem BI WHERE BI.budget_id = :budget_id);
+        """,
+        {"budget_id": budget_id},
+    )
