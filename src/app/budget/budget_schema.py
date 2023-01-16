4e3f2849faba94216a244ab4fa3d4de77e8927e4
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-01-16 18:19:45 +0000

    Add UpdateBudgetRequestSchema

--- src/app/budget/budget_schema.py
+++ src/app/budget/budget_schema.py
@@ -2,7 +2,13 @@ from marshmallow import Schema, fields, post_load
 from ..budget_item.budget_item_schema import BudgetItemSchema
 from ..budget.budget_model import Budget
 
-# class GetBudgetsRequestSchema(Schema):
+
+class GetBudgetRequestSchema(Schema):
+    budget_id = fields.Integer()
+
+    @post_load
+    def make_budget(self, data, **kwargs):
+        return data["budget_id"] if "budget_id" in data else None
 
 
 class GetBudgetResponseSchema(Schema):
@@ -26,3 +32,13 @@ class AddBudgetRequestSchema(Schema):
     @post_load
     def make_budget(self, data, **kwargs):
         return Budget(**data, id=None)
+
+
+class UpdateBudgetRequestSchema(Schema):
+    id = fields.Integer()
+    name = fields.String(required=False, allow_none=True)
+    total_limit = fields.Float(required=False, allow_none=True)
+
+    @post_load
+    def make_budget(self, data, **kwargs):
+        return Budget(**data)
