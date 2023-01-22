ce6d18743b373511f3e33ad3540aae2255ec6c85
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-01-22 12:06:28 +0000

    Init config in main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
-from app.config import CONFIG
+from app import config
 from app import routes
 from app.http import app
 
 if __name__ == "__main__":
+    config.init()
+
     routes.register_routes(app)
 
     from app.database import init
 
     init()
 
-    app.run(debug=CONFIG.DEV, host=CONFIG.HOST)
+    app.run(debug=config.CONFIG.DEV, host=config.CONFIG.HOST)
