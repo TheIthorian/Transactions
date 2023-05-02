ec78d0fc6da501bc822c02c7cbab2c9eb1b5d555
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-05-02 21:02:49 +0100

    feat: add upload schema

---
+++ src/app/uploads/upload_schema.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+from marshmallow import Schema, fields, post_load
+
+from app.uploads.filter import UploadFilter
+
+
+### Get Uploads ###
+
+
+class UploadFilterSchema(Schema):
+    date_from = fields.Date(required=False, allow_none=True)
+    date_to = fields.Date(required=False, allow_none=True)
+
+
+class GetUploadsRequestSchema(UploadFilterSchema):
+    @post_load
+    def make_filter(self, data, **kwargs):
+        return UploadFilter(**data)
+
+
+class GetUploadsResponseSchema(Schema):
+    id = fields.Integer()
+    file_name = fields.String()
+    size = fields.Integer()
+    date = fields.Date()
+    md5 = fields.String()
+
+    class Meta:
+        ordered = True
+
+
+### Add Upload ###
+
+
+class AddUploadSchema(Schema):
+    pass
+
+
+class AddUploadResponseSchema(GetUploadsResponseSchema):
+    pass
