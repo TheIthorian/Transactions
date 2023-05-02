31c0f8f5ab1d137d4b4de978dd129dcc12453b89
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-05-02 21:03:29 +0100

    feat: add file too large error handler

--- src/app/routes/error.py
+++ src/app/routes/error.py
@@ -12,3 +12,16 @@ def make_not_found_error(request):
         return respone
 
     return not_found_error
+
+
+def make_filesize_too_large_error(request):
+    def filesize_too_large_error(error=None):
+        message = {
+            "status": 413,
+            "message": "File too large",
+        }
+        respone = jsonify(message)
+        respone.status_code = 413
+        return respone
+
+    return filesize_too_large_error
